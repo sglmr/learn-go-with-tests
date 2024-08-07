dcbd0160a1e05c23f402c43df2dbdc50ccd11659
stephen gilmore <stephen@gilmore.us>
2024-08-07 13:44:50 -0700

    sum on any slice length working

--- arraysAndSlices/sum_test.go
+++ arraysAndSlices/sum_test.go
@@ -3,14 +3,27 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	t.Parallel()
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		t.Parallel()
 
-	numbers := [5]int{1, 2, 3, 4, 5}
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 15
 
-	if got != want {
-		t.Errorf("got %d want %d given %v", got, want, numbers)
-	}
+		if got != want {
+			t.Errorf("got %d want %d given %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 }
